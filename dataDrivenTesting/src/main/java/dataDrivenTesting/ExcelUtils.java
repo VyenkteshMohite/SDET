6f0c73c03a1aee448aa741bde7a89c894a06b8c6
VyenkteshMohite <mohitevyenktesh@gmail.com>
2024-10-02 15:37:46 +0530

    data driven testing using Excel

---
+++ dataDrivenTesting/src/main/java/dataDrivenTesting/ExcelUtils.java
@@ -0,0 +1,28 @@
+package dataDrivenTesting;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtils {
+
+	//these are the methods objects 
+	
+	public static FileInputStream fi;
+	public static FileInputStream fo;
+	public static XSSFWorkbook wb;
+	public static XSSFSheet ws;
+	public static XSSFRow row;
+	public static XSSFCell cell;
+	public static CellStyle style;
+	
+	
+	
+	
+	
+	
+}
